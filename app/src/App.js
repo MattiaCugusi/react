03303b23a4263115f576798821e38cfbb0abea00
MattiaCugusi <m.cugusi287@gmail.com>
2025-05-12 13:40:53 +0200

    avanzamento con problemi da risolvere

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
-import Square from './Square.js';
+import {useState} from "react";
 function App() {
-  return (
+  const [alunni, setAlunni]=useState([]);
+  const [loading, setLoading]=useState(false);
+  const [inserimento, setIns]=useState(false);
+  function carica(){
+     setLoading(true);
+      fetch('http://localhost:8080/alunni')
+      .then(response => response.json())
+      .then(data => { 
+        setAlunni(data)
+        setLoading(false);
+      });
+    }
+
+    function mostraIns(){
+      setIns(true);
+    }
+
+    function salvaAlunno(){
+      fetch("http://localhost:8080/alunni", {
+        method:"POST",
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify({nome: nome, cognome: cognome})
+      });
+
+      .then(response => response.json())
+      .then(data => { 
+        carica();
+      }
+
+
+    }
+
+
+    return (
     <>
-    <div className="board-row">
-    <Square value="1"/>
-    <Square value="2"/>
-    <Square value="3"/>
-    </div>
-    <div className="board-row">
-    <Square value="4"/>
-    <Square value="5"/>
-    <Square value="6"/>
-    </div>
-    <div className="board-row">
-    <Square value="7"/>
-    <Square value="8"/>
-    <Square value="9"/>
-    </div>
+    <table border="1">
+      {
+        alunni.map(alunno => 
+            <tr>
+              <td>{alunno.id}</td>
+              <td>{alunno.nome}</td>
+              <td>{alunno.cognome}</td>
+            </tr>
+          )
+      }
+    </table>
+    {loading &&
+      <p>caricamento... attendere</p>
+    }
+    {alunni.length===0 && !loading &&
+    <button onClick={carica}>
+      Carica gli alunni
+    </button> 
+    }
+    {alunni.length>0 && !inserimento && 
+    <button onClick={mostraIns}>Inserisci un nuovo alunno</button>
+    }
+    {inserimento && 
+      <form>
+          <input placeholder='Nome' type="text" name="nome" onChange={e => SVGAnimateTransformElement()}></input><br></br>
+          <input placeholder='Cognome'></input><br></br>
+          
+      </form>}
     </>
   );
 }
-
 export default App;
